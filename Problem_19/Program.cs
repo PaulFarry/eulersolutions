a94743b0b2253d0e4ce306bb7e9ad5f59115d376
Paul Farry <PaulFarry@gmail.com>
2017-03-01 09:31:20 +1000

    Added additional problems for testing

---
+++ Problem_19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDate = new DateTime(1901, 1, 1);
+            var endDate = new DateTime(2000, 12, 31);
+
+            var totalSundays = 0;
+
+            var currentDate = startDate;
+
+            while (true)
+            {
+                if (currentDate.Day == 1 && currentDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    totalSundays++;
+                }
+                if (currentDate > endDate)
+                {
+                    break;
+                }
+                currentDate = currentDate.AddMonths(1);
+            }
+
+            Debug.Print($"sundays = {totalSundays}");
+        }
+    }
+}
---
+++ Problem_20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace Problem_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startPoint = 100;
+            BigInteger currentCalculation = 1;
+            while (startPoint > 1)
+            {
+                currentCalculation = currentCalculation * (ulong)startPoint;
+                startPoint--;
+            }
+            Debug.Print($"{currentCalculation}");
+            var digits = currentCalculation.ToString().ToCharArray();
+            var finalResult = 0;
+
+            foreach(var digit in digits)
+            {
+                var digitValue = int.Parse(digit.ToString());
+                finalResult += digitValue;
+            }
+            Debug.Print($"{finalResult}");
+        }
+    }
+}
