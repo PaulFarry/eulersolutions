55a7eea405d6c38ad52713b552217129d82deb2e
Paul Farry <PaulFarry@gmail.com>
2017-03-01 15:38:29 +1000

    added problem 16

---
+++ Problems/Problem16.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Problems
+{
+    class Problem16 : IProblem
+    {
+        public int Number
+        {
+            get
+            {
+                return 16;
+            }
+        }
+
+        public string Execute()
+        {
+            BigInteger startValue = 2;
+
+            for(var i = 1;i<1000;i++)
+            {
+                startValue *= 2;
+            }
+            var value = Utility.SumDigits(startValue.ToString());
+            return value.ToString();
+        }
+    }
+}
