9211213446813294b15eb1487e2da06e2be42446
Paul Farry <PaulFarry@gmail.com>
2017-03-10 17:42:46 +1000

    implemented solution to problem 25

---
+++ Problems/Problem25.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    class Problem25 : IProblem
+    {
+        public int Number => 25;
+        //Find the index of the 1st Fibonacci number to contain 1000 digits.
+
+
+        public string Execute()
+        {
+            var b = new BigInteger(1);
+            var next = new BigInteger(1);
+            var items = new List<BigInteger>();
+            items.Add(b);
+            items.Add(b);
+            while(true)
+            {
+                var c = b + next;
+                b = next;
+                next = c;
+                items.Add(c);
+                if (c.ToString().Length == 1000)
+                {
+                    var resultItem = c;
+                    return items.Count.ToString();
+                }
+            }
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -9,7 +9,7 @@ class Program
     {
         var solvableProblems = FindProblems();
 
-        var problemToRun = 24;
+        var problemToRun = 25;
         RunProblems(solvableProblems, problemToRun);
     }
 
