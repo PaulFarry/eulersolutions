2298d4fda0506fa59b6283350c76343395497758
PaulFarry <paulfarry@gmail.com>
2017-04-01 11:56:10 +1000

    Implemented problem 40

---
+++ Problems/Problems01To49/Problem40.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System.Text;
+
+namespace Problems.Problems01To49
+{
+    class Problem40 : IProblem
+    {
+        public int Number => 40;
+
+        public string Execute()
+        {
+            var maxIndex = 1000000;
+            var currentCounter = 1;
+
+            var characterIndex = 1;
+            var result = 1;
+
+            var indexedChar = new char[1];
+            var sb = new StringBuilder();
+            while (sb.Length < maxIndex)
+            {
+                sb.Append($"{currentCounter}");
+                currentCounter++;
+
+                if (sb.Length > characterIndex)
+                {
+                    sb.CopyTo(characterIndex - 1, indexedChar, 0, 1);
+                    var value = new string(indexedChar);
+                    result *= int.Parse(value);
+                    characterIndex *= 10;
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -10,7 +10,7 @@ class Program
     {
         var solvableProblems = FindProblems();
 
-        var problemToRun = 0;
+        var problemToRun = 40;
         RunProblems(solvableProblems, problemToRun);
     }
 
