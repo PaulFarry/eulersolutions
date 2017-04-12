2bc05c9f91b6df6b7cf4af53d9314388b6cb6a9a
PaulFarry <paulfarry@gmail.com>
2017-04-12 23:03:19 +1000

    Implemented Problem 26, Attempt 1 my solution (a bit slow 6 seconds) Attempt 2 based on the optimised solution in the Euler forums (.5second)

---
+++ Problems/Problems01To49/Problem26.cs
@@ -0,0 +1,96 @@
+﻿using Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Problems.Problems01To49
+{
+    class Problem26 : IProblem
+    {
+        public int Number => 26;
+
+        public string Execute()
+        {
+            var primes = Primes.LoadPrimes(1000);
+
+            //return Attempt1(primes); //Mine
+            return Attempt2(primes); //Euler forums
+        }
+
+        private string Attempt1(List<long> primes)
+        {
+            var bigNumber = BigInteger.Pow(1000, 1000);
+            var longest = 0;
+            var longestValue = (long)0;
+            foreach (var i in primes)
+            {
+                if (i <= 5) continue;
+                var inverseResult = bigNumber / i;
+                var inverse = inverseResult.ToString().TrimEnd('0');
+                var offset = 0;
+                while (offset < inverse.Length / 2)
+                {
+                    var p = FindPattern(inverse.Substring(offset));
+                    if (p != null)
+                    {
+                        if (p.Length > longest)
+                        {
+                            longest = p.Length;
+                            longestValue = i;
+                            break;
+                        }
+                    }
+                    offset++;
+                }
+            }
+            return longestValue.ToString();
+        }
+
+        private string FindPattern(string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            return Enumerable
+                .Range(1, text.Length / 2)
+                .Where(n => text.Length % n == 0)
+                .Select(n => text.Substring(0, n))
+                .Where(pattern => 
+                    Enumerable.Range(0, text.Length / pattern.Length)
+                        .SelectMany(i => pattern)
+                        .SequenceEqual(text)
+                )
+                .FirstOrDefault();
+        }
+
+        private string Attempt2(List<long> primes)
+        {
+            int n, len, maxlen, maxn = 0;
+            maxlen = 0;
+            foreach (var p in primes)
+            {
+                n = (int)p;
+                int rest = 1;
+                for (var i = 0; i < n; i++)
+                {
+                    rest = (rest * 10) % n;
+                }
+                var r0 = rest;
+                len = 0;
+                do
+                {
+                    rest = (rest * 10) % n;
+                    len++;
+                } while (rest != r0);
+                if (len > maxlen)
+                {
+                    maxn = n;
+                    maxlen = len;
+                }
+            }
+            return maxn.ToString();
+        }
+    }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -11,7 +11,7 @@ class Program
 
         var solvableProblems = ProblemService.GetProblems(typeof(Problems.DiscoveryProblem).Assembly);
 
-        var problemToRun = 0;
+        var problemToRun = 26;
         RunProblems(solvableProblems, problemToRun);
     }
 
