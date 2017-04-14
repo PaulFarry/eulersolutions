a1e64d6df89533ba285e6a3a5dc1a6d3cb8d1105
PaulFarry <paulfarry@gmail.com>
2017-04-14 23:01:11 +1000

    initial problem 98, I don't understand the CARE -> 1296 conversion

---
+++ Problems/Problems50To99/Problem98.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Problems.Problems50To99
+{
+
+    class Problem098 : IProblem
+
+    {
+        public int Number => 98;
+
+        public string Execute()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var nameSpace = "Problems";
+            var resourceName = $"{nameSpace}.DataFiles.p98.txt";
+            var wordList = new List<string>();
+
+            var data = string.Empty;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                while (true)
+                {
+                    var word = reader.ReadLine();
+                    if (string.IsNullOrEmpty(word)) break;
+                    wordList.Add(word);
+                }
+            }
+
+            throw new ProblemIncompleteException("How on earth does 'CARE' become 1296");
+        }
+    }
+}
