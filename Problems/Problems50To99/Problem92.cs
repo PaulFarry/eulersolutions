e8bd1f0661fa0cc3564874bc9ad4037146711230
Paul Farry <PaulFarry@gmail.com>
2017-04-19 10:21:07 +1000

    solved problem 92

--- Problems/Problems50To99/Problem92.cs
+++ Problems/Problems50To99/Problem92.cs
@@ -1,10 +1,6 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Problems.Problems50To99
 {
@@ -12,63 +8,111 @@ namespace Problems.Problems50To99
     {
         public int Number => 92;
 
+        private HashSet<int> SolvedValues;
+        private HashSet<int> Bucket89;
+
         public string Execute()
         {
+
             var max = (int)Math.Pow(10, 7);
             var totalValues = 0;
 
-            var sb = new StringBuilder();
-            //var startPoint = 44;
+            Bucket89 = new HashSet<int>();
+            SolvedValues = new HashSet<int>();
+
             var chains = new Dictionary<int, HashSet<int>>();
-            for (var startPoint = 1; startPoint < max; startPoint++)
-            //   var startPoint = 85;
+            var subMax = SumSquareDigits(max - 1);  // Find out maximum starting reduced start point.
+
+            for (var startPoint = 1; startPoint < subMax; startPoint++)
             {
-            
-                var chainValue = startPoint;
-                if (chainValue == 89) continue;
-                var chain = new HashSet<int>();
-                //sb.Clear();
-                while (true)
+                totalValues = CalculateChain(totalValues, startPoint);
+            }
+
+            for (var startPoint = subMax; startPoint < max; startPoint++)
+            {
+                totalValues = CalculateChain(totalValues, startPoint);
+            }
+
+
+            return totalValues.ToString();
+        }
+
+        private int CalculateChain(int totalValues, int startPoint)
+        {
+            var chainValue = startPoint;
+            var chain = new List<int>();
+            while (true)
+            {
+                var newChainValue = SumSquareDigits(chainValue);
+
+                if (SolvedValues.Contains(newChainValue))
                 {
-                    var newChainValue = SumSquareDigits(chainValue);
-                    //if (chains.Contains(newChainValue))
+                    if (Bucket89.Contains(newChainValue))
                     {
-                        //  chain = chains[newChainValue];
+                        totalValues++;
                     }
-                    if (chain.Contains(newChainValue))
+                    break;
+                }
+                else
+                {
+                    if (chain.Contains(newChainValue) && (newChainValue == 1 || newChainValue == 89))
                     {
                         chain.Add(newChainValue);
 
                         if (newChainValue == 89)
                         {
                             totalValues++;
-                            //     Debug.Print($"{startPoint} = {chain.Count}");
+                            foreach (var value in chain)
+                            {
+                                if (value > 10)
+                                {
+                                    var reverse = value.ToString().ToCharArray();
+                                    Array.Reverse(reverse);
+
+                                    var altValue = int.Parse(string.Join("", reverse));
+                                    Bucket89.Add(altValue);
+
+                                }
+                                Bucket89.Add(value);
+                            }
                         }
-                        /*
-                        sb.Append(startPoint);
-                        foreach (var v in chain)
+
+                        foreach (var value in chain)
                         {
-                            sb.Append(" > ");
-                            sb.Append(v);
+                            SolvedValues.Add(value);
                         }
-                        sb.Append(" > ").Append(newChainValue);
-                        Debug.Print(sb.ToString());
-                        */
                         break;
                     }
                     chain.Add(newChainValue);
                     chainValue = newChainValue;
                 }
             }
-            return totalValues.ToString(); ;
+
+            return totalValues;
         }
 
-        private int SumSquareDigits(int startPoint)
+        private Dictionary<int, int> squaresLut;
+
+        private int SumSquareDigits(int value)
         {
-            var sum = 0;
-            foreach (var c in startPoint.ToString().ToCharArray())
+            if (squaresLut == null)
             {
-                sum += (int)Math.Pow(int.Parse(c.ToString()), 2);
+                squaresLut = new Dictionary<int, int>();
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var square = (int)Math.Pow(i, 2);
+                    var ch = i.ToString().ToCharArray()[0];
+                    squaresLut.Add(i, square);
+                }
+            }
+
+            var sum = 0;
+            while (value > 0)
+            {
+                var d = value % 10;
+                value /= 10;
+                sum += squaresLut[d];
             }
             return sum;
         }
