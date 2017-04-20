72f12f87ea64f87dfe10a430b7656852330bae02
Paul Farry <PaulFarry@gmail.com>
2017-04-20 17:17:34 +1000

    inprogress

---
+++ Problems/Problems150To199/Problem152.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Problems150To199
+{
+    class Problem152 : IProblem
+    {
+        public int Number => 152;
+
+        public string Execute()
+        {
+            var lcm = Enumerable.Range(2, 79).Select(x => ((BigInteger)x * x).ToList();
+
+        }
+    }
+}
